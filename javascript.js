e0fb079b8c66be13554cc67a4ac1f4b90399912c
Ilche <ile.spasesky@gmail.com>
2020-10-12 16:14:21 +0200

    update

--- javascript.js
+++ javascript.js
@@ -9,3 +9,37 @@ window.addEventListener('scroll',()=> {
 })
 
 
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+} 
+
+
+  
+  $(document).ready(function(){
+    $(".header-text").animate({opacity: '0.5'}, "slow");
+    $(".header-text").animate({opacity: '0.7'}, "slow");
+    $(".header-text").animate({opacity: '0.9'}, "slow");
+
+    $(".header-text").animate({opacity: '1'}, "slow");
+
+      
+    
+  });
+  $(document).ready(function(){
+    $(".about-content").animate({opacity: '0.5'}, "slow");
+    $(".about-content").animate({opacity: '0.7'}, "slow");
+    $(".about-content").animate({opacity: '0.9'}, "slow");
+
+    $(".header-text").animate({opacity: '1'}, "slow");
+
+      
+    
+  });
+  
