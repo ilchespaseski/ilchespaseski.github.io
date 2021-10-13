bc436f8ece8c1d21309c35d2e44be8e3ca4ca6e8
Ilche <ile.spasesky@gmail.com>
2021-10-13 22:10:20 +0200

    update final

--- javascript.js
+++ javascript.js
@@ -1,6 +1,26 @@
-$(window).scroll(()=>{
-  $(".navbar").css("background-color", "white");
-  if(scroll()===0) {
-     console.log("bblbbll")
-  }
+$(document).ready(function () {
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+
+  $(".nav-link").on("click", function () {
+    $(".navbar-collapse").toggle();
+  });
+  $(".navbar-toggler-icon").on("click", function () {
+    $(".navbar-collapse").toggle();
+  });
 });
